bf58de6f46bda2e228f54dd6beba86e9c7db94fc
HeleneHF <helenehf@stud.ntnu.no>
2017-11-04 17:29:48 +0100

    Tester mot vårt program

--- EldreProsjekter/MariMichelleSiri/Kode/endeM.m
+++ EldreProsjekter/MariMichelleSiri/Kode/endeM.m
@@ -4,7 +4,7 @@ function [endemoment, moment_rotasjon] = endeM(nelem, elem,...
 endemoment = zeros(nelem,2);%Totale enemoment
 moment_rotasjon = zeros(nelem,2);%Bjelkeendemoment skapt av rotasjon
     %Skal brukes i beregning av skjear kraft
-    
+ bjelkestivhet; 
 for i = 1:nelem
     
     %Henter ut knutepunktnummer for element
@@ -16,10 +16,10 @@ for i = 1:nelem
     vinkel2 = rot(punkt2);
     
     %Beregner belkeendemoment fra rotasjoner
+    bjelkestivhet(i)
     moment_rotasjon(i,:) = bjelkestivhet(i) *...
-        [4 * vinkel1 + 2 * vinkel2, 2 * vinkel1 + 4 * vinkel2];
-    fim(i,1)
-    fim(i,2)
+        [4 * vinkel1 + 2 * vinkel2, 2 * vinkel1 + 4 * vinkel2]
+    %fim(i,:)
     %Bjelkeendemomentene fra rotasjoner settes i matrise, sammen med
     %fastinnspenningsmomentene
     endemoment(i,:) = fim(i,:) + moment_rotasjon(i,:);
